f75d7fdc5b47a3b752308b92b12364b1b59183e5
Reynaldo Esparza <reynaldoeg_esparza@hotmail.com>
2023-06-02 21:05:38 -0600

    Create page

---
+++ Controller/Index/Index.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace ChupaPrecios\TechnicalTest\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    private PageFactory $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    )
+    {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    public function execute()
+    {
+        $resultPageFactory = $this->resultPageFactory->create();
+
+        // Add page title
+        $resultPageFactory->getConfig()->getTitle()->set(__('Chupaprecios'));
+
+        // Add breadcrumb
+        /** @var \Magento\Theme\Block\Html\Breadcrumbs */
+        $breadcrumbs = $resultPageFactory->getLayout()->getBlock('breadcrumbs');
+        $breadcrumbs->addCrumb('home',
+            [
+                'label' => __('Home'),
+                'title' => __('Home'),
+                'link' => $this->_url->getUrl('')
+            ]
+        );
+        $breadcrumbs->addCrumb('chupaprecios',
+            [
+                'label' => __('Example'),
+                'title' => __('Example')
+            ]
+        );
+
+        return $resultPageFactory;
+    }
+}
